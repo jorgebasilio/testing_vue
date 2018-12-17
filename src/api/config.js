a5fa5e04110ddeff47f5c62cd623b2826031ab48
Jorge Basilio <jbasilio@loganmedia.mobi>
2018-12-17 09:53:48 +0000

    first commit

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+// Application name 	Platzi Music
+// API key 	2068dd7fcfa2e08547b8b2ddfae8e408
+// Shared secret 	cda58fdc10e3bd6f3f54549edcf0ba40
+// Registered to 	JorgeBasilio
+export default {
+  appName: 'Platzi Music',
+  apiKey: '2068dd7fcfa2e08547b8b2ddfae8e408',
+  secret: 'cda58fdc10e3bd6f3f54549edcf0ba40',
+  registerTo: 'JorgeBasilio'
+};
